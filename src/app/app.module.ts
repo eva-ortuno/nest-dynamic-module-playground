1d7bd8e191de9e381e11dfea67badfb0cae6aa22
Eva Ortuno <ortuno.eva.pro@gmail.com>
2024-02-14 16:45:30 +0100

    Shift to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthService } from './auth/auth.service';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
   imports: [
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { AuthUserService } from './auth/auth-user.service';
+import { AuthUserService } from '../auth/auth-user.service';
 
 @Injectable()
 export class AppService {
